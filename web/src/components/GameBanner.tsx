bb8923f35c13af1bb4068b55d2f6bbaffc8110fe
liara987 <987.liara@gmail.com>
2022-09-25 15:44:59 -0300

    chore: fixing background gradient

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,17 +1,19 @@
-export interface GameBannerProps {  
+interface GamesBannerProps {
   title: string;
   bannerUrl: string;
   adsCount: number;
 }
 
-export function GameBanner({ bannerUrl, title, adsCount }: GameBannerProps) {
-  return (
-      <a className="relative rounded-lg overflow-hidden keen-slider__slide" href="#">
-          <img src={bannerUrl} alt={title} />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-              <strong className='font-bold text-white'>{title}</strong>
-              <span className='text-sm text-zinc-300 block'>{adsCount} anúncio(s)</span>
-          </div>
+export function GameBanner(props: GamesBannerProps){
+  return(
+      <a className="relative rounded-lg overflow-hidden" href="">
+        <img src={props.bannerUrl} alt={props.title} />
+        <div className="w-full pt-16 pb-4 px-4 bg-gradient-box absolute bottom-0 left-0 right-0">
+          <strong className="font-bold text-white block">
+            {props.title}
+          </strong>
+          <span className="text-zinc-300 block">{props.adsCount} anúncio(s)</span>
+        </div>
       </a>
   )
 }
